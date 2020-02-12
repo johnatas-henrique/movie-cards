95866d30001bdd16c427ed43ed4e4a21841a2d0f
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-02-12 17:08:10 -0300

    Requisitos 1 a 4

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: '',
+      showLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    movieAPI.getMovies().then((respPromise) =>
+      this.setState({
+        movies: respPromise,
+        showLoading: false,
+      }),
+    );
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, showLoading } = this.state;
 
     // Render Loading here if the request is still happening
+    if (showLoading) return <Loading />;
 
     return (
-      <div className="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+      <div>
+        <div className="movie-list">
+          {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        </div>
       </div>
     );
   }
