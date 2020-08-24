0e83c792ce090ea9badfafcdee94c94a4641ef75
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-08-24 00:52:31 -0300

    Updates

---
+++ precache-manifest.5ed17efec8d943c324ace6ea2b330127.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f7c9ce10de26bd9b69058c2b13fa828",
+    "url": "/repository/index.html"
+  },
+  {
+    "revision": "f6cf0044e63c6cd74fa3",
+    "url": "/repository/static/css/main.1bd77eaf.chunk.css"
+  },
+  {
+    "revision": "362874d626ac8f55b163",
+    "url": "/repository/static/js/2.2ab8b186.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/repository/static/js/2.2ab8b186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6cf0044e63c6cd74fa3",
+    "url": "/repository/static/js/main.00d85961.chunk.js"
+  },
+  {
+    "revision": "3440fb8570a1b76e1b88",
+    "url": "/repository/static/js/runtime-main.9ce720f8.js"
+  }
+]);
